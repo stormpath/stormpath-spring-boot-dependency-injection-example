f186797fc3918e0c197abbdd37eb64343606d890
Micah Silverman <micah@afitnerd.com>
2016-07-25 23:16:02 -0400

    initial commit

---
+++ src/main/java/com/stormpath/example/DependencyInjectionApplication.java
@@ -0,0 +1,12 @@
+package com.stormpath.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyInjectionApplication.class, args);
+    }
+}
