5ae633f4345ac73f1702d8b6ca6d7d94963cab9a
Micah Silverman <micah@afitnerd.com>
2016-07-27 15:40:19 -0400

    Added /beans to show Spring managed beans

--- src/main/java/com/stormpath/example/controller/HomeController.java
+++ src/main/java/com/stormpath/example/controller/HomeController.java
@@ -2,11 +2,21 @@ package com.stormpath.example.controller;
 
 import com.stormpath.example.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HomeController {
+
+    @Autowired
+    ApplicationContext appContext;
 
     @Autowired
     GreetingService greetingService;
@@ -14,5 +24,20 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         return greetingService.greet();
+    }
+
+    @RequestMapping("/beans")
+    public @ResponseBody Map<String, String[]> beans(@RequestParam(required = false) String q) {
+        Map<String, String[]> retMap = new HashMap<>();
+
+        String[] retArray = Arrays.stream(appContext.getBeanDefinitionNames())
+            .filter(beanName ->
+                (q == null || q.length() == 0) ||
+                beanName.toLowerCase().contains(q.trim().toLowerCase())
+            )
+            .toArray(String[]::new);
+
+        retMap.put("beans", retArray);
+        return retMap;
     }
 }
